7b00646df7725b167b9479f552aab25b3ef70832
Ratheesh24807 <ratheeshagowda@gmail.com>
2022-08-06 14:20:33 +0530

    Take 3 user inputs

---
+++ sortingarray.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "welcome to shell scripting program: sorting an array";
+
+read - "Enter the value of a: "a;
+read - "Enter the value of b: "b;
+read - "Enter the value of c: "c;
